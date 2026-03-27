8221fdb84f63ccee6f383a560f61afdac0725a06
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-03-27 11:58:44 +0100

    Add 1st task

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ * Return: number of characters printed
+ */
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
+	char c;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
